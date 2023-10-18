249ee31383ad09b7629313430d87785eadd39d53
thomasim22 <thomasim@clarkson.edu>
2023-10-18 18:16:25 -0400

    Revert "Javadoc for User, Response, GamingResponse classes and methods"
    This reverts commit b475525753609d205ebb7796553a734763e09696.

--- src/socket/Response.java
+++ src/socket/Response.java
@@ -1,40 +1,22 @@
 package socket;
 import java.lang.String;
 
-/**
- * @author kailisacco
- * @version Response.java v1
- */
 public class Response {
     public ResponseStatus status;
     public String message;
 
-    /**
-     * @return returns ResponseStatus type object indicative of the status of server response
-     */
     public ResponseStatus getStatus() {
         return status;
     }
 
-    /**
-     * @param status The status of server response. It of type ResponseStatus
-     */
     public void setStatus(ResponseStatus status) {
         this.status = status;
     }
 
-    /**
-     * @return returns string message description about the
-     *         status of the client-server communication
-     */
     public String getMessage() {
         return message;
     }
 
-    /**
-     * @param message assigns string message description about the
-     *                status of the client-server communication.
-     */
     public void setMessage(String message) {
         this.message = message;
     }
@@ -44,18 +26,11 @@ public class Response {
         FAILURE
     }
 
-    /**
-     *  default constructor for Response class
-     */
     public Response(){
         this.status = ResponseStatus.FAILURE;
         this.message = "Request FAILURE";
     }
 
-    /**
-     * @param status    Of type ResponseStatus, either FAILURE or SUCCESS
-     * @param message   String type message, representing the status of the response
-     */
     public Response(ResponseStatus status, String message){
         this.status = status;
         this.message = message;
