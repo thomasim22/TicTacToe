a772087a71266814bf66df4f8fc14ff1661941ea
kailisacco <56237788+kailisacco@users.noreply.github.com>
2023-10-18 16:22:51 -0400

    Response.java population

--- src/socket/Response.java
+++ src/socket/Response.java
@@ -1,4 +1,38 @@
 package socket;
+import java.lang.String;
 
 public class Response {
+    private ResponseStatus status;
+    private String message;
+
+    public ResponseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ResponseStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    enum ResponseStatus{
+        SUCCESS,
+        FAILURE
+    }
+
+    public Response(){
+        this.status = ResponseStatus.FAILURE;
+        this.message = "Default Message";
+    }
+
+    public Response(ResponseStatus status, String message){
+        this.status = status;
+        this.message = message;
+    }
 }
