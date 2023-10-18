71938bbb6cdafe602d9b34984cc85fe99ec8e13a
thomasim22 <thomasim@clarkson.edu>
2023-10-18 18:36:56 -0400

    Defining Javadocs

--- src/server/SocketServer.java
+++ src/server/SocketServer.java
@@ -2,8 +2,14 @@ package server;
 
 public class SocketServer {
 
+    /**
+     * @param PORT is an integer representing the last move made by the current player's opponent
+     */
     private int PORT;
 
+    /**
+     * The static main method that instantiates the classes setup() and startAcceptingRequest()
+     */
     public static void main(String[] args){
     }
 
@@ -15,15 +21,24 @@ public class SocketServer {
         // To be implemented in further milestones
     }
 
+    /**
+     * Default constructor that calls the default port value of 5000
+     */
     public SocketServer(){
         this.PORT = 5000;
     }
 
+    /**
+     * @return PORT parameter
+     */
     public int getPORT(){
         return PORT;
     }
 
-    public void setPORT(Integer PORT){
+    /**
+     * Sets the constant port number
+     */
+    public void SocketServer(int PORT){
         this.PORT = PORT;
     }
 
