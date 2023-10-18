86be132365be3ce4b3e73de5ff5fb78e9381463f
thomasim22 <thomasim@clarkson.edu>
2023-10-18 18:11:26 -0400

    Defining Classes

--- src/server/SocketServer.java
+++ src/server/SocketServer.java
@@ -2,8 +2,15 @@ package server;
 
 public class SocketServer {
 
+    /**
+     * @param PORT is an integer representing the last move made by the current player's opponent.
+     *             The value 0-8 represents the cell of TicTacToe from top-bottom, left-right
+     */
     private int PORT;
 
+    /**
+     * Static main method instantiates the classes setup() and startAcceptingRequest()
+     */
     public static void main(){
 
         public setup(){
@@ -14,15 +21,24 @@ public class SocketServer {
         }
     }
 
+    /**
+     * A default constructor that calls the parameterized constructor with a default port value of 5000
+     */
     public SocketServer(){
         this.PORT = 5000;
     }
 
+    /**
+     * @return the PORT attribute gotten
+     */
     public int getPORT(){
         return PORT;
     }
 
-    public void setPORT(Integer PORT){
+    /**
+     * A constructor that sets the port number
+     */
+    public void SocketServer(int PORT){
         this.PORT = PORT;
     }
 
