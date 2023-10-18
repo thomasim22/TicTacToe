51812cb40a704650ca38166e5cd86d85aaf094e8
kailisacco <56237788+kailisacco@users.noreply.github.com>
2023-10-18 17:19:42 -0400

    Response.java implementation GamingResponse.java implementation ServerHandler implementation

--- src/server/ServerHandler.java
+++ src/server/ServerHandler.java
@@ -1,4 +1,14 @@
 package server;
 
-public class ServerHandler {
+public class ServerHandler extends Thread{
+    public ServerHandler(){
+    }
+
+    @Override
+    public void run(){
+    }
+
+    public void close(){
+    }
+
 }
